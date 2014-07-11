449105a1071806e7554474cd319d162bc1a23c65
mininao <maxence_aici@yahoo.fr>
2014-07-11 08:14:43 +0200

    Added existing project

---
+++ app/js/app.js
@@ -0,0 +1,40 @@
+var app = (function(document, $) {
+
+	'use strict';
+	var docElem = document.documentElement,
+
+		_userAgentInit = function() {
+			docElem.setAttribute('data-useragent', navigator.userAgent);
+		},
+		_init = function() {
+			$(document).foundation();
+			_userAgentInit();
+		};
+
+	return {
+		init: _init
+	};
+
+})(document, jQuery);
+
+(function() {
+
+	'use strict';
+	app.init();
+    
+
+})();
+
+
+$(document).ready(function(){
+    'use strict';
+	$('.jumbotron').slick({
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:4000,
+        lazyLoad:'progressive',
+        dots: false,
+        fade: true,
+        speed: 500
+	});
+});
